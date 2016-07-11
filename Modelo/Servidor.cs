549ea5ee870fdee8fda9b7b530b99038560f822c
German <electronik.32@gmail.com>
2016-07-11 17:14:26 -0300

    Revert "Revert "Actualizacion_1 11/07/16""
    This reverts commit 10392002967b3c9e195a6336e3bf98ff4fe23ea8.

--- Modelo/Servidor.cs
+++ Modelo/Servidor.cs
@@ -5,27 +5,15 @@ namespace Modelo
     /// <summary>
     /// Entidad Servidor, modela un servidor de correo. Ej: gmail, yahoo, hotmail.
     /// </summary>
-    public class Servidor : IEntidadModelo
+    public class Servidor
     {
-        //Identificador del servidor.
-        public int Id { get; set; }
         //Nombre del servidor.
         public string Nombre { get; set; }
-        //Host smtp del servidor.
-        public string HostSMTP { get; set; }
-        //Host pop3 del servidor.
-        public string HostPOP { get; set; }
-        //Puerto smtp del servidor.
-        public int PuertoSMTP { get; set; }
-        //Puerto pop3 del servidor.
-        public int PuertoPOP { get; set; }
-        //SSL, capa de puertos de seguridad, del servidor.
-        public bool SSL { get; set; }
-        //Colección de Cuentas asociadas al servidor.
-        public virtual ICollection<Cuenta> Cuenta { get; set; }
+        //Protocolos de comunicacion del servidor
+        public IEnumerable<Protocolo> Protocolos { get; set; }
+
         public Servidor()
         {
-            Cuenta = new List<Cuenta>();
         }
     }
 }
